625913739ddf851b909dffaad37b8cf5380a26b5
kusnadi99 <muh.kusnadi99@gmail.com>
2022-04-14 06:28:09 +0800

    add input helper

---
+++ Helper/InputHelper.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Helper;
+
+class InputHelper {
+
+    static function input(string $info): string {
+
+        echo "$info : ";
+        $result = fgets(STDIN);
+        return trim($result);
+
+    }
+
+}
