c6143ee123df1498ff85d40a92d660b6a92d0b49
kusnadi99 <muh.kusnadi99@gmail.com>
2022-04-14 06:28:40 +0800

    TodolistView logic

--- View/TodolistView.php
+++ View/TodolistView.php
@@ -2,21 +2,71 @@
 
 namespace View;
 
+use Service\TodolistService;
+use Helper\InputHelper;
+
 class TodolistView {
+
+    private TodolistService $todolistService;
+
+    public function __construct(TodolistService $todolistService)
+    {
+        $this->todolistService = $todolistService;
+    }
 
     function showTodolist(): void
     {
+        while (true) {
+            
+            $this->todolistService->showTodolist();
 
+            echo "MENU" . PHP_EOL;
+            echo "1. Tambah Todo" . PHP_EOL;
+            echo "2. Hapus Todo" . PHP_EOL;
+            echo "x. Keluar" . PHP_EOL;
+
+            $pilihan = InputHelper::input("Pilih");
+
+            if ($pilihan == "1") {
+                $this->addTodolist();
+            } elseif ($pilihan == "2") {
+                $this->removeTodolist();
+            } elseif ($pilihan == "x") {
+                break;
+            } else {
+                echo "Pilihan tidak dimengerti" . PHP_EOL;
+            }
+
+        }
+
+        echo "Sampai Jumpa Lagi" . PHP_EOL;
     }
 
     function addTodolist(): void
     {
+        echo "MENAMBAH TODO" . PHP_EOL;
 
+        $todo = InputHelper::input("Todo (x untuk batal)");
+
+        if ($todo == "x") {
+            echo "Batal menambah todo" . PHP_EOL;
+        } else {
+            $this->todolistService->addTodolist($todo);
+        }
+        
     }
 
     function removeTodolist(): void
     {
+        echo "MENGHAPUS TODO" . PHP_EOL;
 
+        $todo = InputHelper::input("Todo (x untuk batal)");
+
+        if ($todo == "x") {
+            echo "Batal menambah todo" . PHP_EOL;
+        } else {
+            $this->todolistService->removeTodolist($todo);
+        }
     }
 
 }
